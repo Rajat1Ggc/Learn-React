76fcb2c1f2eab7654c6a396aeef2496e5e21fc6b
Rajat <rajat1ggc@gmail.com>
2023-04-29 17:03:37 +0530

    useState

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from 'react';
+
 import ExpenceDate from "./expenceDate";
 import './Home.css';
+
+
 function Home(props) {
+  const [Newtitle, setNewTitle] = useState("");
+  const [title, setTitle] = useState(props.title);
+
+  const ClickHendelr = () => {
+    setTitle(Newtitle);
+  }
+  const changehendle = (event) => {
+    setNewTitle(event.target.value);
+  }
   return (
     <div className="expence-item">
       <ExpenceDate date={props.date} />
       <div className="expence-item_description">
-        <h2>{props.title}</h2>
-        <button className="expence-item_price">
+        <h2>{title}</h2>
+        <div className="expence-item_price">
           ${props.price}
-        </button>
+        </div>
+        <input type='text' value={Newtitle} onChange={changehendle} />
+        <button onClick={ClickHendelr}>change Title</button>
       </div>
     </div>
   );
